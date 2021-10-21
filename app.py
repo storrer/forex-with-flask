82ca78c6e1c174db70027329a0c427c009c07baa
Chris Storrer <storrer.chris@gmail.com>
2021-10-21 13:25:06 -0700

    Remove Debug Toolbar, update requirements

--- app.py
+++ app.py
@@ -1,27 +1,22 @@
 # Christopher Storrer's Assessment
 from flask import Flask, request, render_template, flash, session, redirect
 from rates_and_codes import convert_currency, validate_user_input
-# remove later
-from flask_debugtoolbar import DebugToolbarExtension
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "oh-so-secret"
-app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
-debug = DebugToolbarExtension(app)
 
 
 
+# global results variable, should be a session variable, not sure how to implement this
 RESULTS = {}
 
-# List of valid currency codes for form validation
-# if code not in valid_currency_codes then raise exception
 
 
 
 @app.route("/")
 def homepage_form():
     """Shows the homepage with the foreign currency converter form."""
-    # Exception that must be removed later, here for debugging only
-    #raise
+   
     return render_template("form-page.html")
 
 @app.route("/calculate", methods= ["POST"])
@@ -38,9 +33,5 @@ def evaluate_currency():
     amount = request.form['amount']
     RESULTS = convert_currency(from_currency, to_currency, amount)
    
-    #return redirect("/results")
     return render_template("results-page.html",results=RESULTS)
 
-#@app.route("/results")
-#def display_results():
-#    """Display the results of the currency conversion process"""
