b5e95859d4765e7ec80a393e4ae1bc810f215ba2
chrisstorrer <storrer.chris@gmail.com>
2021-10-21 00:43:01 -0700

    Removed duplicate line `app = Flask(__name__)`

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 # Christopher Storrer's Assessment
 from flask import Flask, request, render_template
+# Our currency rate getter
+from forex_python.converter import CurrencyRates
+""" Usage Examples from forex-python.readthedocs.io
+        >>> c = CurrencyRates()
+        >>> c.get_rates('USD')   # you can directly call get_rates('USD')
+        {u'IDR': 13625.0, u'BGN': 1.7433, ...}
+        get_rates
+
+
+
+"""
 
 # remove later
 from flask_debugtoolbar import DebugToolbarExtension
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+
+debug = DebugToolbarExtension(app)
+
+
 
 
 # List of valid currency codes for form validation
@@ -13,7 +30,6 @@ valid_currency_codes = ["EUR", "IDR", "BGN", "ILS", "GBP", "DKK", "CAD",
                         "THB", "USD", "NOK", "RUB", "INR", "MXN", "CZK",
                         "BRL", "PLN", "PHP", "ZAR"]
 
-app = Flask(__name__)
 
 @app.route("/")
 def homepage_form():
